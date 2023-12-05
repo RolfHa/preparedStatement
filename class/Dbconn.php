8b215924bf84d478937d9d2df43be49908d8f251
Rolf Häckel <Your new email>
2023-12-05 10:51:06 +0100

    db - anschluss mittels Konstanten

--- class/Dbconn.php
+++ class/Dbconn.php
@@ -4,6 +4,7 @@ abstract class Dbconn
 {
     public static function getConn():PDO
     {
-        return  new PDO("mysql:host=".SERVERNAME.";dbname=". DB_NAME, USERNAME, PASS);
+        return  new PDO("mysql:host=".SERVERNAME.";dbname=".
+            DB_NAME, USERNAME, PASS);
     }
 }
